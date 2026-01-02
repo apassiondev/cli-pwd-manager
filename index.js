9f074c94955738c3ed68755e0e0005395013461e
Andrea Ho <anhhothai.iamcoder@gmail.com>
2026-01-02 22:09:28 +0700

    feat: finalize the level-1 operations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import chalk from "chalk";
 import promptModule from "prompt-sync";
 
 // instantiate prompt to use its async-await functionality
@@ -6,95 +7,125 @@ const prompt = promptModule();
 // Define an object to represent the local database
 const mockDB = { passwords: {} };
 
+const menuText = [
+  "[1] View passwords",
+  "[2] Manage new password",
+  "[3] Verify password",
+  "[4] Exit",
+];
+
 const saveNewPassword = (password) => {
   mockDB.hash = bcrypt.hashSync(password, 10);
-  console.log("Password has been saved!");
+  console.log(chalk.green("Password has been saved!"));
   showMenu();
 };
 
 // define a custom func to compare a plain-text password to a hashed password
-const compareHashedPassword = async (password) => {
-  // compare the input password to the value in your local database
-  return await bcrypt.compare(password, mockDB.hash);
+const compareHashedPassword = (plainPassword) => {
+  // compare the input plainPassword to the value in your local database
+  return bcrypt.compareSync(plainPassword, mockDB.hash);
 };
 
 const promptNewPassword = () => {
-  const response = prompt("Enter the main password: ");
-  return saveNewPassword(response);
+  const response = prompt(
+    "Enter the main password: ",
+    {
+      echo: "*",
+    } /* hide the input completely with specifying a character to task the input  */
+  );
+  saveNewPassword(response);
 };
 
-const promptOldPassword = async () => {
+const promptOldPassword = () => {
   let verified = false; // the flag that tracks whether the password has been verified
 
   while (!verified) {
+    console.log("Verify?:", verified);
     // prompt user to retype their existing password
-    const response = prompt("Enter your password: ");
+    const response = prompt("Enter your current password: ", { echo: "*" });
     // compare the input against the stored hashed password
-    const result = await compareHashedPassword(response);
 
-    if (result) {
-      console.log("Password verified.");
-      //  set verification flag to `true` once the password is validated
-      verified = true;
-      //  Show menu if the password is correct.
-      showMenu();
-    } else {
-      // Display an error and retry if the password is incorrect
-      console.log("Password incorrect. Try again.");
+    try {
+      const result = compareHashedPassword(response);
+      console.log("Result:", result);
+
+      if (result) {
+        console.log(chalk.green("Password verified!"));
+        //  set verification flag to `true` once the password is validated
+        verified = true;
+        //  Show menu if the password is correct.
+        showMenu();
+      } else {
+        // Display an error and retry if the password is incorrect
+        console.log(chalk.red("Password incorrect! Try again."));
+      }
+    } catch (error) {
+      throw new Error(error);
     }
   }
 };
 
-const showMenu = async () => {
+const showMenu = () => {
   // prompt the user with 4 options to select
-  console.log(`1. View passwords\n2. Manage new password\n3. Verify password\n4. Exit
-    `);
-  const response = prompt(">");
-  switch (response) {
-    case "1":
+  console.log(`${menuText.join("\n")}`);
+  const response = prompt("Choose an option: ");
+  switch (parseInt(response)) {
+    case 1:
       viewPasswords();
       break;
-    case "2":
+    case 2:
       promptManageNewPassword();
       break;
-    case "3":
+    case 3:
       promptOldPassword();
       break;
-    case "4":
-      // if no valid option is selected, the user is prompted again
+    case 4:
       process.exit();
     default:
-      console.log("That's an invalid response.");
+      // if no valid option is selected, navigate to the main menu.
+      console.log(chalk.red("That's an invalid response."));
       showMenu();
   }
 };
 
 const viewPasswords = () => {
-  const { passwords } = mockDB;
+  const passwordEntries = Object.entries(mockDB.passwords || {});
 
-  Object.entries(passwords).forEach(([key, value], index) => {
-    console.log(`${index + 1}. ${key} => ${value}`);
-  });
+  if (passwordEntries.length) {
+    passwordEntries.forEach(([key, value], index) => {
+      console.log(`${index + 1}. ${key} => ${value}`);
+    });
+    return;
+  }
 
+  console.log(chalk.yellow("No passwords found!".toUpperCase()));
   showMenu();
 };
 
 const promptManageNewPassword = () => {
-  const source = prompt("Enter title for password");
-  const password = prompt("Enter password to save");
+  const source = prompt("Enter title for password: ");
+  const password = prompt("Enter password to save: ", { echo: "*" });
 
   // save the source and password pair in mockDB
   mockDB.passwords[source] = password;
 
-  console.log(`Password for ${source} has been saved!`);
+  console.log(
+    chalk.green(`Password for "${source}" has been saved!`.toUpperCase())
+  );
 
-  // to display the menu options
   showMenu();
 };
 
 // Start the app
-if (!mockDB.hash) {
-  promptNewPassword();
-} else {
-  promptOldPassword();
-}
+const main = async () => {
+  // If there is no existing hash value
+  if (!mockDB.hash) {
+    promptNewPassword();
+  } else {
+    promptOldPassword();
+  }
+
+  showMenu();
+};
+
+main();
